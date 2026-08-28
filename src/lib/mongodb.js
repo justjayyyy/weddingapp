7c3fa1a2c7c6c2873ae884f1408d48008526f5e9
Jenia <jenia.meresidi@bankleumi.co.il>
2026-08-28 22:54:53 +0300

    Safely handle missing MONGODB_URI during build

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -6,23 +6,24 @@ const options = {};
 let client;
 let clientPromise;
 
-// We will throw the error when trying to connect instead of at the top level
-if (!process.env.MONGODB_URI) {
+if (!uri) {
   console.warn('MONGODB_URI is missing. Please add it to your environment variables.');
-}
-
-if (process.env.NODE_ENV === 'development') {
-  // In development mode, use a global variable so that the value
-  // is preserved across module reloads caused by HMR (Hot Module Replacement).
-  if (!global._mongoClientPromise) {
-    client = new MongoClient(uri, options);
-    global._mongoClientPromise = client.connect();
-  }
-  clientPromise = global._mongoClientPromise;
+  // Return a dummy promise to prevent build errors when the variable is missing
+  clientPromise = Promise.resolve(null);
 } else {
-  // In production mode, it's best to not use a global variable.
-  client = new MongoClient(uri, options);
-  clientPromise = client.connect();
+  if (process.env.NODE_ENV === 'development') {
+    // In development mode, use a global variable so that the value
+    // is preserved across module reloads caused by HMR (Hot Module Replacement).
+    if (!global._mongoClientPromise) {
+      client = new MongoClient(uri, options);
+      global._mongoClientPromise = client.connect();
+    }
+    clientPromise = global._mongoClientPromise;
+  } else {
+    // In production mode, it's best to not use a global variable.
+    client = new MongoClient(uri, options);
+    clientPromise = client.connect();
+  }
 }
 
 export default clientPromise;
